da9cb097dd2d01831b084550fc3d5e44cbc92e13
mitchroy41 <mitchelr0777@student.faytechcc.edu>
2017-06-05 18:55:52 -0400

    Add files via upload
    First submission

---
+++ M2HW1_DistanceTraveled_Mitchell.py
@@ -0,0 +1,12 @@
+# Calculates distance traveled.
+# 6/5/2017
+# CTI-110 M2HW1 - Distance Traveled 
+# Roy Mitchell
+# Get number of hours traveled.
+hours_traveled=float(input('Enter number of hours traveled: '))
+# Calculate distance traveled.
+distance_traveled=70*hours_traveled
+# Display distance traveled.
+print ('The distance traveled is',distance_traveled)
+
+       
